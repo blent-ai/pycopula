22394597eaab59f1423f5942a908b2a8a201dbdb
Maxime <maxime.jumelle@etu.upmc.fr>
2018-05-22 23:35:24 +0200

    Doc bug fixes.

--- pycopula/examples.py
+++ pycopula/examples.py
@@ -2,30 +2,31 @@ import sys
 
 sys.path.insert(0, '..')
 
-import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import cm
-from pycopula.copula import *
-from mpl_toolkits.mplot3d import Axes3D
-from pycopula.visualization import pdf_2d
+from pycopula.copula import ArchimedeanCopula
+from pycopula.visualization import concentrationFunction
 from pycopula.simulation import simulate
 
-# The Gaussian copula withb specified covariance matrix
-gaussian = GaussianCopula(dim=2, sigma=[[1, 0.8], [0.8, 1]])
-# Visualization of CDF and PDF
-u, v, c = pdf_2d(gaussian)
-X, Y = np.meshgrid(u, v)
+# The Clayton copula
+clayton = ArchimedeanCopula(dim=2, family='clayton')
 
-# Sampling of size 1000 with Gaussian copula
-sim = simulate(gaussian, 1000)
+# Sampling of size 1000 with Clayton copula
+sim = simulate(clayton, 1000)
+
+# Computing theoritical and empirical concentrations functions
+downI, upI, tailDown, tailUp = concentrationFunction(sim)
+ClaytonDown = [ clayton.concentrationDown(x) for x in downI ]
+ClaytonUp = [ clayton.concentrationUp(x) for x in upI ]
 
 # Plotting
-fig = plt.figure()
-ax = fig.add_subplot(111, title="Gaussian copula PDF")
-ax.contour(X, Y, c, levels = np.arange(0,5,0.15), alpha=0.5)
-plt.scatter([ s[0] for s in sim ], [s[1] for s in sim ], alpha=0.6, edgecolors='none')
-plt.title("Simulation of 1000 points with Gaussian copula")
-plt.xlim(0, 1)
-plt.ylim(0, 1)
+plt.figure()
+plt.plot(downI, tailDown, color='red', linewidth=3, label="Empirical concentration")
+plt.plot(upI, tailUp, color='red', linewidth=3)
+plt.plot(downI, ClaytonDown, color='blue', linewidth=1, label="Clayton concentration")
+plt.plot(upI, ClaytonUp, color='blue', linewidth=1)
+plt.plot([0.5, 0.5], [0, 1], color='gray', alpha=0.6, linestyle='--', linewidth=1)
+plt.title("Lower-Upper tail dependence Coefficients")
+plt.xlabel("Lower Tail      Upper Tail")
+plt.legend(loc=0)
 
 plt.show()
