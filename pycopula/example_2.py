e2e8e2129f058cdb921ec078b0a4556d08d04a7e
MaximeJumelle <jumelle.maxime@gmail.com>
2018-11-06 17:06:05 +0100

    Logo

--- pycopula/example_2.py
+++ pycopula/example_2.py
@@ -1,4 +1,5 @@
 import sys
+import time
 
 sys.path.insert(0, '..')
 
@@ -9,16 +10,38 @@ from pycopula.visualization import pdf_2d, cdf_2d, concentrationFunction
 from pycopula.simulation import simulate
 from pycopula.copula import ArchimedeanCopula, GaussianCopula
 
+import numpy as np
+import matplotlib.pyplot as plt
+
 data = pd.read_csv("mydata.csv").values[:,1:]
 
 print("Begin")
-archimedean = ArchimedeanCopula(family="clayton", dim=2)
+archimedean = GaussianCopula(dim=2)
+elapsedTime = time.time()
 archimedean.fit(data, method="cmle")
+elapsedTime = time.time() - elapsedTime
+print(elapsedTime)
+
+
+u, v, C = cdf_2d(archimedean)
+u, v, c = pdf_2d(archimedean)
+
+fig = plt.figure()
+ax = fig.add_subplot(121, title="Clayton copula CDF")
+X, Y = np.meshgrid(u, v)
+
+
+ax = fig.add_subplot(122, title="Clayton copula PDF")
+ax.contour(X, Y, c, levels = np.arange(0,5,0.2))
+
+plt.show()
+
+sys.exit()
 print("End")
 print(archimedean)
 
 clayton = ArchimedeanCopula(family="clayton", dim=2)
-boundAlpha = [0, 1] # Greater than 0
+boundAlpha = [0, None] # Greater than 0
 boundLambda = [0, 0.5]
 bounds = [ boundAlpha, boundLambda ]
 paramX1 = { 'a': None, 'scale': 1.2 } # Hyper-parameters of Gamma
